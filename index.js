626476ecfe0d15daf935c6c05c117a4fdf17c633
Ryan Gates <Ryan_Gates@ultimatesoftware.com>
2019-02-17 00:44:06 -0500

    got docker working and serving back content

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/1', (req, res) => res.send('1'));
+app.get('/2', (req, res) => res.send('2'));
+app.get('/3', (req, res) => res.send('3'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
