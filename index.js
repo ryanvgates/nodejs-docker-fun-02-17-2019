b9c45ee1923a305b645eb62e85cf9b079551305f
Ryan Gates <Ryan_Gates@ultimatesoftware.com>
2019-02-18 23:11:51 -0500

    added console log to test out docker logs command

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    console.log(`responding to request ${req} at ${new Date().toLocaleString()}`);
+    res.send('Hello World!');
+});
 app.get('/1', (req, res) => res.send('1'));
 app.get('/2', (req, res) => res.send('2'));
 app.get('/3', (req, res) => res.send('3'));
